11a2357a42cfcdaadbae4dc7744747dad3806ff3
Saurav <sauravkumar00108@gmail.com>
2024-04-27 23:38:26 +0530

    Update dependencies and remove unused import in Table.js

--- src/component/Table.js
+++ src/component/Table.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { makeStyles } from '@mui/styles';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 170, possition: { sticky: 'true' }},
