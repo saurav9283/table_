5cdb10d2cef268b7ed44e7ffcd054216b9f9aef1
Saurav <sauravkumar00108@gmail.com>
2024-04-27 23:34:57 +0530

    Add @mui/styles dependency and update table column properties in Table.js

--- src/component/Table.js
+++ src/component/Table.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { makeStyles } from '@mui/styles';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 170,sticky: true },
-  { field: 'firstName', headerName: 'First name', width: 170,sticky: true },
+  { field: 'id', headerName: 'ID', width: 170, possition: { sticky: 'true' }},
+  { field: 'firstName', headerName: 'First name', width: 170, sticky: true },
   { field: 'lastName', headerName: 'Last name', width: 170 },
   {
     field: 'age',
@@ -36,35 +37,32 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon',address:"Delhi", age: 35, Number: "098766789" },
-  { id: 2, lastName: 'Snow', firstName: 'Jon',address:"Delhi", age: 35, Number: "098766789" },
-  { id: 3, lastName: 'Snow', firstName: 'Jon',address:"Delhi", age: 35, Number: "098766789" },
-  { id: 4, lastName: 'Snow', firstName: 'Jon',address:"Delhi", age: 35, Number: "098766789" },
-  { id: 5, lastName: 'Snow', firstName: 'Jon',address:"Delhi", age: 35, Number: "098766789" },
-  { id: 6, lastName: 'Snow', firstName: 'Jon',address:"Delhi", age: 35, Number: "098766789" },
-  // { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  // { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  // { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  // { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  // { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  // { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  // { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  // { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, lastName: 'Snow', firstName: 'Jon', address: "Delhi", age: 35, number: "098766789" },
+  { id: 2, lastName: 'Snow', firstName: 'Jon', address: "Delhi", age: 35, number: "098766789" },
+  { id: 3, lastName: 'Snow', firstName: 'Jon', address: "Delhi", age: 35, number: "098766789" },
+  { id: 4, lastName: 'Snow', firstName: 'Jon', address: "Delhi", age: 35, number: "098766789" },
+  { id: 5, lastName: 'Snow', firstName: 'Jon', address: "Delhi", age: 35, number: "098766789" },
+  { id: 6, lastName: 'Snow', firstName: 'Jon', address: "Delhi", age: 35, number: "098766789" },
 ];
 
 export default function DataTable() {
+  // const classes = useStyles();
+
+  const [selectedRows, setSelectedRows] = React.useState([]);
+
+  const handleSelectionChange = (newSelection) => {
+    setSelectedRows(newSelection.selectionModel);
+  };
+
+
   return (
     <div style={{ height: 400, width: '80%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
         pageSizeOptions={[5, 10]}
         checkboxSelection
+        onSelectionModelChange={handleSelectionChange}
       />
     </div>
   );
